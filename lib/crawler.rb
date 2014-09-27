ead9776ab2a8567ebf07220de9a0a58c90be8f8a
Wellington Santos <wjsantos.wj@gmail.com>
2014-09-26 23:24:47 -0300

    encode options for reading iso-8859-1

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -3,6 +3,9 @@ class Crawler
     url = 'http://ww2.mobilicidade.com.br/bikesampa/mapaestacao.asp'
     response = HTTParty.get(url)
 
+    encode_options = { :undef => :replace, :invalid=>:replace, :replace=>"?" }
+    response = response.encode("iso-8859-1", encode_options).encode('UTF-8')
+
     Parser::Sampa.parse(response)
   end
 end
